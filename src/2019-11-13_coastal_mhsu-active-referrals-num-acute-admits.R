7da2b3dbb00e97e7f42f517a93b76d88732c9f44
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-11-14 00:20:04 -0800

    filter admit/disch nursing units - only include rows where either admit or disch is to an Acute unit

--- src/2019-11-13_coastal_mhsu-active-referrals-num-acute-admits.R
+++ src/2019-11-13_coastal_mhsu-active-referrals-num-acute-admits.R
@@ -28,10 +28,20 @@ cnx <<- DBI::dbConnect(odbc::odbc(), dsn = "cnx_SPDBSCSTA001")
 vw_paris_ref <- dplyr::tbl(cnx, dbplyr::in_schema("[CommunityMart].[dbo]", 
                                                   "[vwPARISReferral]"))
 
+vw_nunit_classification <- dplyr::tbl(cnx, dbplyr::in_schema("[ADRMart].[Dim]", 
+                                                             "[LocationGrp]"))
+
 #+ rest
 
+#' # Todo: 
+#' 
+#' 
+
+#' # Data 
+#' 
+#' 
 d1_max_referral <- 
-  vw_paris_ref %>% 
+  vw_paris_ref %>%  # [vwPARISReferral]
   filter(CommunityProgramGroup == "Mental Health & Addictions", 
          CommunityRegion %in% c("Coastal Urban",
                                 "Coastal Rural")) %>% 
@@ -40,6 +50,32 @@ d1_max_referral <-
   pull(ReferralDate) %>% 
   max()
 
+
+#' Nursing unit classification: 
+#' 
+
+df0.n_units <- 
+  vw_nunit_classification %>%  # [ADRMart].[Dim].[LocationGrp]
+  collect() %>% 
+  
+  # add col is_acute
+  mutate(is_acute = case_when((IsHospice == 0 &
+                                IsTCU == 0 & 
+                                IsTMH == 0 & 
+                                IsRcExtended == 0 & 
+                                IsRehab == 0 & 
+                                IsDaycare == 0) ~ 1,
+                              TRUE ~ 0)) %>% 
+  filter(is_acute == 1)
+
+df0.n_units %>% 
+  datatable(extensions = 'Buttons',
+            options = list(dom = 'Bfrtip', 
+                           buttons = c('excel', "csv")))
+                           
+
+#' All patients in PARISReferral with MHSU program, from Coastal: 
+#' 
 
 df1.paris_mhsu <- 
   vw_paris_ref %>% 
@@ -58,16 +94,37 @@ df1.paris_mhsu <-
 #             options = list(dom = 'Bfrtip', 
 #                            buttons = c('excel', "csv")))
                            
+sites_param <- c("VGH", 
+                 "UBCH",
+                 "LGH", 
+                 "PRGH",
+                 "SSH", 
+                 "SGH",
+                 "SPH", 
+                 "MSJ")
+
+#' All inpatient either admit or discharge encounters at "regional hospitals"
+#' since 2015-01-01. Only looking at units classified as "isAcute" 
+#' 
 
 df2.admits <- 
-  vw_admission_discharge %>% 
+  vw_admission_discharge %>%  # publish.admission_discharge
   filter(encntr_type_class_grp_at_ad == "Inpatient", 
-         admit_date_id >= "20150101") %>% 
+         admit_date_id >= "20150101", 
+         (facility_short_name_at_admit %in% sites_param |
+            facility_short_name_at_disch %in% sites_param)) %>% 
   select(patient_id, 
          admit_date_id, 
-         facility_short_name_at_admit) %>% 
-  collect()
+         facility_short_name_at_admit, 
+         facility_short_name_at_disch ,
+         nursing_unit_desc_at_admit, 
+         nursing_unit_desc_at_disch) %>% 
+  collect() %>% 
+  filter((nursing_unit_desc_at_admit %in% df0.n_units$LocationGrpDescription | 
+            nursing_unit_desc_at_disch %in% df0.n_units$LocationGrpDescription))
 
+#' Now join the 2 datasets: 
+#' 
 
 # join mhsu referrals and all-sites admits 
 df3.mhsu_admits <- 
@@ -79,12 +136,19 @@ df3.mhsu_admits <-
   mutate(admit_date = ymd(admit_date_id))
 
 # view: 
-index <- 1900  # index for slicing
+index <- sample(30000, 1)  # index for slicing
 df3.mhsu_admits %>% 
   slice(index:(index+99)) %>% 
   datatable(extensions = 'Buttons',
             options = list(dom = 'Bfrtip', 
                            buttons = c('excel', "csv")))
+
+#' Next, we have to filter to make sure that either the admit or disch n_unit is
+#' in the is_acute category: 
+#' 
+
+  
+
 
 #' # Plots 
 #' 
@@ -111,3 +175,28 @@ df4.mhsu_admits_by_day %>%
 
   
             
+
+#' # Appendix 
+#' 
+#' These are the queries for pulling data: 
+#' 
+# CommunityMart PARISReferral View: 
+vw_paris_ref %>% 
+  filter(CommunityProgramGroup == "Mental Health & Addictions", 
+         CommunityRegion %in% c("Coastal Urban",
+                                "Coastal Rural")) %>% 
+  select(PatientID, 
+         CommunityProgramGroup) %>% 
+  show_query()
+
+# denodo admission_discharge: 
+vw_admission_discharge %>% 
+  filter(encntr_type_class_grp_at_ad == "Inpatient", 
+         admit_date_id >= "20150101", 
+         (facility_short_name_at_admit %in% sites_param |
+            facility_short_name_at_disch %in% sites_param)) %>% 
+  select(patient_id, 
+         admit_date_id, 
+         facility_short_name_at_admit, 
+         facility_short_name_at_disch) %>% 
+  show_query()
